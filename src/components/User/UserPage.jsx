296752df5b5b915da71cfbaaaf3a61de89c05526
Darshan <priyadarshanmanoharan@gmail.com>
2023-07-29 09:26:43 -0700

    DarkKnight-Movie

--- src/components/User/UserPage.jsx
+++ src/components/User/UserPage.jsx
@@ -14,17 +14,21 @@ const UserPage = () => {
         <div className="container-fluid p-0">
           <div className="first">
             <h1>The Batman's EndGame!</h1>
-            <h2 className="gfont text-center" id='textpg1'>Crime can't hide from the watchful eyes of Batman – Report and reveal!</h2>
+            <h2 className="gfont text- mb-5 pb-5" id='textpg1'>Crime can't hide from the watchful eyes of Batman – Report and reveal!</h2>
             <Link to="/Register"><button  className="become-batman-btn">Join</button></Link>
           </div>
         </div>
         <div className="container-fluid p-0">
           <div className="second text-center">
-            <h2 className="gfont text-center" id='textpg1'>
+            <div className='row container'>
+              <div className='col-lg-6'>
+            <h2 className="gfont text-center  " id='textpg1'>
               Be the hero Batman needs – report crime today!</h2>
-            <button id='btnstyle' className="btnreport bg-transparent" onClick={() => setShowModal(true)}>
+            <button id='btnstyle' className="btnreport bg-transparent " onClick={() => setShowModal(true)}>
               <span>Report</span>
             </button>
+            </div>
+            </div>
           </div>
         </div>
         <div>
